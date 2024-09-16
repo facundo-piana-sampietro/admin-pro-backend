c904bc7beed5f327cc7fe43aa19b92c41dafeb0d
Facundo Piana Sampietro <facusampi@hotmail.com>
2024-09-16 16:26:12 -0300

    Fin sección 12: Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express')
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -34,7 +35,7 @@ const login = async(req, res = response) => {
         const token = await generarJWT(usuarioDB.id);
 
         res.json({
-            ok: false,
+            ok: true,
             token
         })
 
@@ -47,6 +48,48 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async(req, res = response) => {
+
+    try {
+        const {email, name, picture} = await googleVerify(req.body.token)
+
+        const usuarioDB = await Usuario.findOne({email});
+        let usuario;
+
+        if (!usuarioDB){
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB
+            usuario.google = true;
+        }
+
+        //Guardar usuario
+        await usuario.save();
+
+        //Generar el TOKEN - JWT
+        const token = await generarJWT(usuarioDB.id);
+
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'El token de google no es correcto'
+        })
+    }
+}
+
 module.exports = {
     login,
+    googleSignIn
 }
